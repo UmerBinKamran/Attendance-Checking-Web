b9c1f7fb1673e7866651c9b3d15a572804d2eeca
UmerBinKamran <umerbinkamran@gmail.com>
2025-12-04 21:02:53 +0500

    Login System complete

---
+++ login.js
@@ -0,0 +1,21 @@
+function login() {
+  var emailInput = document.getElementById("email").value;
+  var passwordInput = document.getElementById("password").value;
+
+  var storedEmail = "admin@gmail.com";
+  var storedPassword = "admin123";
+
+  if (emailInput === storedEmail && passwordInput === storedPassword) {
+    alert("Login Successful!");
+
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid Email or Password");
+  }
+}
+var button = document.getElementById("loginBtn");
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    login();
+  }
+});
